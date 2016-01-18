d8b57c8b5dad4767ef8c7227be25c89e07d16dfa
Paul <vixtir90@gmail.com>
2016-01-18 23:45:45 +0300

    hound corrections 4

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,13 +1,13 @@
 class Card < ActiveRecord::Base
   scope :rand_word, -> { order("RANDOM()").limit(1) }
-  scope :need_check, -> { where('review_date <= ?', Time.zone.now) }
+  scope :need_check, -> { where("review_date <= ?", Time.zone.now) }
 
   before_validation :set_review_date, on: [:create]
   before_save :downcase_translate
 
   validates :original_text, :translated_text, :review_date, presence: true
   validates :translated_text, format: { with: /\A[а-яА-Я]+\z/,
-                                        message:"Только на кириллице" }
+                                        message: "Только на кириллице" }
   validate  :equal_text
 
   def equal_text
@@ -18,7 +18,7 @@ class Card < ActiveRecord::Base
   end
 
   def downcase_translate
-    self.translated_text = self.translated_text.mb_chars.downcase
+    self.translated_text = translated_text.mb_chars.downcase
   end
 
   def set_review_date
