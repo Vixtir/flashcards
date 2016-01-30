ec26738d71f71796df83f89874d5f6ce0c54f17d
Paul <vixtir90@gmail.com>
2016-01-30 17:06:20 +0300

    correct

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -57,7 +57,7 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   def login(email, password)
-    visit '/login'
+    visit login_path 
     fill_in :email, with: email
     fill_in :password, with: password
     click_button "Login"
