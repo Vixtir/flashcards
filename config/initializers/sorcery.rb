60b4ac88879a5c48f04d8c0a4f32802b02adbe49
Paul <vixtir90@gmail.com>
2016-01-31 22:01:56 +0300

    hound corr

--- config/initializers/sorcery.rb
+++ config/initializers/sorcery.rb
@@ -6,7 +6,7 @@ Rails.application.config.sorcery.configure do |config|
   config.facebook.secret = ENV["facebook_secret"]
   config.facebook.callback_url = "http://0.0.0.0:3000/oauth/callback?provider=facebook"
   config.facebook.user_info_mapping = { email: "name" }
-  config.facebook.access_permissions = ["email", "publish_actions"]
+  config.facebook.access_permissions = %W[email publish_actions]
   config.facebook.display = "page"
   config.facebook.api_version = "v2.2"
 
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -3,7 +3,7 @@ require "capybara/rspec"
 RSpec.configure do |config|
   config.expect_with :rspec do |expectations|
     expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  endi
+  end
 
   config.mock_with :rspec do |mocks|
     mocks.verify_partial_doubles = true
