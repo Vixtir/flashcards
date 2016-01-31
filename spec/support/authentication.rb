946632aeabad0501f936f3f9479922136e1965c0
Paul <vixtir90@gmail.com>
2016-01-31 16:29:44 +0300

    travis

--- spec/support/authentication.rb
+++
@@ -1,8 +0,0 @@
-module AuthenticationForFeatureRequest
-  def login user, password = 'login'
-    user.update_attributes password: password
-
-    page.driver.post sessions_url, {email: user.email, password: password}
-    visit root_url
-  end
-end
