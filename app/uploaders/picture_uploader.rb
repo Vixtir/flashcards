27d3d6381e867a2e6651e16f71d844d83dad484b
Paul <vixtir90@gmail.com>
2016-02-03 23:33:27 +0300

    trav spec

--- app/uploaders/picture_uploader.rb
+++ app/uploaders/picture_uploader.rb
@@ -2,11 +2,11 @@
 class PictureUploader < CarrierWave::Uploader::Base
   include CarrierWave::RMagick
 
- # if Rails.env.test?
-   # storage :file
- # else
-  storage :fog
- # end
+  if Rails.env.test?
+    storage :file
+  else
+    storage :fog
+  end
 
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
