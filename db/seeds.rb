375d9177c86270224e5bd80855d5e52cf0562274
Paul <vixtir90@gmail.com>
2016-02-15 22:38:33 +0300

    CC

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,5 +7,5 @@ doc = Nokogiri::HTML(open(url))
 doc.css('#middle table tr:not(:first-child)').each do |e|
   o_text = e.css("td")[1].text
   t_text = e.css("td")[3].text
-  card = Card.create(:original_text => o_text, :translated_text => t_text)
+  Card.create(:original_text => o_text, :translated_text => t_text)
 end
