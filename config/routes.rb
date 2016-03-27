9d27e3dad1188582c41733b2b90f2adaad096f70
Paul <vixtir90@gmail.com>
2016-03-28 00:04:01 +0300

    namespaces

--- config/routes.rb
+++ config/routes.rb
@@ -2,27 +2,30 @@ Rails.application.routes.draw do
   scope "(:locale)", locale: /en|ru/ do
     get '/:locale' => 'home#index'
     root "dashboard/home#index"
+
     namespace :home do
-        post "oauth/callback" => "oauths#callback"
-        get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
-        get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
-    	root "home#index"
-        resources :users, only: [:new, :create]
-        resources :user_sessions, only: [:new, :create]       
-        get "login" => "user_sessions#new", :as => :login
+      root "home#index"
+
+      post "oauth/callback" => "oauths#callback"
+      get "oauth/callback" => "oauths#callback" # for use with Github, Facebook
+      get "oauth/:provider" => "oauths#oauth", :as => :auth_at_provider
+      get "login" => "user_sessions#new", :as => :login
+
+      resources :users, only: [:new, :create]
+      resources :user_sessions, only: [:new, :create]
     end
 
     namespace :dashboard do
-    	root "home#index"
+      root "home#index"
 
-	   	patch "/decks/:id" => "decks#activate", :as => :activate
-	   	post "check" => "home#check"
-	   	post "logout" => "user_sessions#destroy", :as => :logout
+      patch "/decks/:id" => "decks#activate", :as => :activate
+      post "check" => "home#check"
+      post "logout" => "user_sessions#destroy", :as => :logout
 
-	   	resources :cards
-	   	resources :users, only: [:edit, :update, :destroy]
-        resources :user_sessions, only: :destroy
-	   	resources :decks
+      resources :cards
+      resources :users, only: [:edit, :update, :destroy]
+      resources :user_sessions, only: :destroy
+      resources :decks
     end
   end
 end
