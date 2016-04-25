50fec7c60581ce80183d00c01f2f3dfce1d4b6da
Paul <vixtir90@gmail.com>
2016-04-25 22:30:34 +0300

    tests

--- spec/features/user_spec.rb
+++ spec/features/user_spec.rb
@@ -4,20 +4,19 @@ describe "User", type: "feature" do
   let(:user) { FactoryGirl.build(:user) }
 
   describe "enter on the site first time" do
-    it "default english language" do
-      visit root_path
-      expect(page).to have_content "Flashcards"
-    end
+    context 'when the user has set their locale to :en' do
+      let(:locale) { :en }
 
-    it "change language" do
-      visit root_path
-      click_link("Русский")
-      expect(page).to have_content "Флешкарточкер"
+      it "he can change language to russian" do
+        visit root_path
+        click_link("Русский")
+        expect(page).to have_content "Флешкарточкер"
+      end
     end
 
     it "has a link for registration" do
       visit root_path
-      expect(page).to have_content "Регистрация"
+      expect(page).to have_content I18n.t('not_registred.link')
     end
 
     it "have no logout link" do
@@ -27,7 +26,7 @@ describe "User", type: "feature" do
 
     it "has a Facebook login link" do
       visit root_path
-      expect(page).to have_content "Зайти через Facebook"
+      expect(page).to have_content I18n.t('main.facebook_login')
     end
   end
 
@@ -39,7 +38,7 @@ describe "User", type: "feature" do
       fill_in "user_password", with: user.password
       fill_in "user_password_confirmation", with: user.password_confirmation
       click_button "Create User"
-      expect(page).to have_content "Добро пожаловать"
+      expect(page).to have_content I18n.t('flash.user.create')
     end
 
     it "path has html5 validation" do
@@ -56,15 +55,15 @@ describe "User", type: "feature" do
     end
 
     it "valid" do
-      expect(page).to have_content "Добро пожаловать"
+      expect(page).to have_content I18n.t('flash.session.login')
     end
 
     it "has a logout button" do
-      expect(page).to have_content "Выйти"
+      expect(page).to have_content I18n.t('main.logout')
     end
 
     it "has a profile link" do
-      expect(page).to have_content "Профиль"
+      expect(page).to have_content I18n.t('main.profile')
     end
 
     it "enter into profile" do
